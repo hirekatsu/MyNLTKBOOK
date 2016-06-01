cea7411208e0611d26db8870784171a4aae73c15
hirekatsu <izumibobcat1@gmail.com>
2016-06-02 08:08:05 +0900

    ch09 draft

---
+++ ch09_01.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import division
+import nltk, re, pprint
+from nltk import word_tokenize
+
+print("""
+----------------------------------------------------------------------
+9.  Building Feature Based Grammars
+----------------------------------------------------------------------
+1  Grammatical Features
+----------------------------------------------------------------------
+""")
+
